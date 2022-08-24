aae0c809d13af4afbf87c16762cb3fd7f79131d5
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-08-24 23:17:52 +0200

    helloWebApp added

---
+++ helloWebApp/helloWebApp/src/main/java/io/github/shahroozsabet/HelloServlet.java
@@ -0,0 +1,23 @@
+package io.github.shahroozsabet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Created by Shahrooz on 8/24/2022.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        request.setAttribute("username", "hello " + username);
+        request.getRequestDispatcher("response.jsp").forward(request, response);
+    }
+}
