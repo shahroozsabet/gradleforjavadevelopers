d12848bffd68a8a7ee857d0799a5964e7cdbb895
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-08-28 10:07:24 +0200

    springbootdemo2

---
+++ springbootdemo2/src/test/java/io/github/shahroozsabet/gradle/Springbootdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shahroozsabet.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
